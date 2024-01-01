35eeb6873f0a7a1cd899e5fd639d9d605988af57
Gabriel P <gabopos@gmail.com>
2023-12-31 20:23:42 -0500

    Upload Change InnerHTML attributes

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,13 @@
 const d = document;
-
-d.querySelector("button").addEventListener("click", handleClick);
-function handleClick() {
-  alert("I got clicked");
-}
+// d.querySelector("button").addEventListener("click", (e) => {
+//   alert("I got clicked");
+// });
+const element = d.querySelectorAll(".drum");
+// console.log(element);
+element.forEach((element) => {
+  element.addEventListener("click", function() {
+    // var audio = new Audio("./sounds/tom-1.mp3");
+    // audio.play();
+    console.log(this.style.color = "white");
+  });
+});
