002577986e8935a39a7ae14f49866d5ac61a9b87
Gabriel P <gabopos@gmail.com>
2024-01-08 17:18:58 -0500

    start project Simon's game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,5 +1,11 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 3 + 1);
-  
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  console.log(gamePattern);
 }
+
