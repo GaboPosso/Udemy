cec1d63cd6a28ebcf3ae01d2012068632043c35b
Gabriel P <gabopos@gmail.com>
2024-01-09 23:41:42 -0500

    update Simons Game with Jquery and animation

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,10 +2,33 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 
 function nextSequence() {
-
-  var randomNumber = Math.floor(Math.random() * 3 + 1);
+  var randomNumber = Math.floor(Math.random() * (3 + 1));
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  console.log(gamePattern);
-}
 
+  for (let i = 0; i < gamePattern.length; i++) {
+    switch (gamePattern[i]) {
+      case "red":
+        $("#red").animate({opacity:0.5}, 500);
+        // console.log("this selection is Red")
+
+        break;
+      case "blue":
+        $("#blue").animate({opacity:0.5}, 500);
+        // console.log("this selection is blue")
+        break;
+      case "green":
+        $("#green").animate({opacity:0.5}, 500);
+        // console.log("this selection is Green")
+        break;
+      case "yellow":
+        $("#yellow").animate({opacity:0.5}, 500);
+        // console.log("this selection is Yellow")
+        break;
+      default:
+        break;
+    }
+
+    // console.log(gamePattern);
+  }
+}
