34a50e235e6deb3100dbbbb4fc16f1cfbeec41a0
Gabriel P <gabopos@gmail.com>
2024-01-11 23:58:38 -0500

    Upload Simon exercise blink buttons

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -9,20 +9,20 @@ function nextSequence() {
   for (let i = 0; i < gamePattern.length; i++) {
     switch (gamePattern[i]) {
       case "red":
-        $("#red").animate({opacity:0.5}, 500);
+        $("#red").fadeOut(100).fadeIn(100);
         // console.log("this selection is Red")
 
         break;
       case "blue":
-        $("#blue").animate({opacity:0.5}, 500);
+        $("#blue").fadeOut(100).fadeIn(100);
         // console.log("this selection is blue")
         break;
       case "green":
-        $("#green").animate({opacity:0.5}, 500);
+        $("#green").fadeOut(100).fadeIn(100);
         // console.log("this selection is Green")
         break;
       case "yellow":
-        $("#yellow").animate({opacity:0.5}, 500);
+        $("#yellow").fadeOut(100).fadeIn(100);
         // console.log("this selection is Yellow")
         break;
       default:
