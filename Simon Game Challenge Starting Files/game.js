9be610c86f2a079b08356591982c626c460d38b7
Gabriel P <gabopos@gmail.com>
2024-01-12 00:21:07 -0500

    Upload Simon exercise blink buttons

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -11,18 +11,25 @@ function nextSequence() {
       case "red":
         $("#red").fadeOut(100).fadeIn(100);
         // console.log("this selection is Red")
-
+        var audioRed = new Audio('./sounds/red.mp3');
+        audioRed.play();
         break;
       case "blue":
         $("#blue").fadeOut(100).fadeIn(100);
+        var audioBlue = new Audio('./sounds/blue.mp3');
+        audioBlue.play();
         // console.log("this selection is blue")
         break;
       case "green":
         $("#green").fadeOut(100).fadeIn(100);
+        var audioGreen = new Audio('./sounds/green.mp3');
+        audioGreen.play();
         // console.log("this selection is Green")
         break;
       case "yellow":
         $("#yellow").fadeOut(100).fadeIn(100);
+        var audioYellow = new Audio('./sounds/yellow.mp3');
+        audioYellow.play();
         // console.log("this selection is Yellow")
         break;
       default:
