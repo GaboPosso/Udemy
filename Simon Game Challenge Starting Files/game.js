e46a9fefc7cdf3c6efd62243a04d2533079722a3
Gabriel P <gabopos@gmail.com>
2024-01-12 23:42:29 -0500

    Upload button handler Simon's game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,41 +1,28 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * (3 + 1));
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  for (let i = 0; i < gamePattern.length; i++) {
-    switch (gamePattern[i]) {
-      case "red":
-        $("#red").fadeOut(100).fadeIn(100);
-        // console.log("this selection is Red")
-        var audioRed = new Audio('./sounds/red.mp3');
-        audioRed.play();
-        break;
-      case "blue":
-        $("#blue").fadeOut(100).fadeIn(100);
-        var audioBlue = new Audio('./sounds/blue.mp3');
-        audioBlue.play();
-        // console.log("this selection is blue")
-        break;
-      case "green":
-        $("#green").fadeOut(100).fadeIn(100);
-        var audioGreen = new Audio('./sounds/green.mp3');
-        audioGreen.play();
-        // console.log("this selection is Green")
-        break;
-      case "yellow":
-        $("#yellow").fadeOut(100).fadeIn(100);
-        var audioYellow = new Audio('./sounds/yellow.mp3');
-        audioYellow.play();
-        // console.log("this selection is Yellow")
-        break;
-      default:
-        break;
-    }
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    // console.log(gamePattern);
-  }
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+
+  $(".btn").click(function() {
+
+    //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+    var userChosenColour = $(this).attr("id");
+  
+    //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+    userClickedPattern.push(userChosenColour);
+  
+    console.log(userClickedPattern);
+  
+  });
+  
 }
