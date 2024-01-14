4c9659506ddf278e46953af19e390aed53a28d5b
Gabriel P <gabopos@gmail.com>
2024-01-14 16:01:39 -0500

    Upload Simon's exercise - Button pressed

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,6 +2,12 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 var userClickedPattern = [];
 
+
+ $(".btn").click(function() {
+  
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+ })
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * (3 + 1));
   var randomChosenColour = buttonColours[randomNumber];
@@ -13,16 +19,6 @@ function nextSequence() {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 
-  $(".btn").click(function() {
 
-    //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
-    var userChosenColour = $(this).attr("id");
-  
-    //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
-    userClickedPattern.push(userChosenColour);
-  
-    console.log(userClickedPattern);
-  
-  });
   
 }
