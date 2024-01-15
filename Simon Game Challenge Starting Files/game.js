5cb418b4d2e5afe59f83519701da4f5283311b90
Gabriel P <gabopos@gmail.com>
2024-01-14 23:39:35 -0500

    Upload button detecter

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,24 +1,32 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-var userClickedPattern = [];
+let userClickedPattern = [];
 
-
- $(".btn").click(function() {
-  
+$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
- })
+
+  playSound(userChosenColour);
+});
+
+//3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * (3 + 1));
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-
-
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  
+  playSound(randomChosenColour);
   
 }
+  function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
+
+
