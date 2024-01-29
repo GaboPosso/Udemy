e28544bda3271c1f6674b31a765348b31d16e647
Gabriel P <gabopos@gmail.com>
2024-01-28 21:58:08 -0500

    Upload HTML requests exercise and insall nodemon

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -2,6 +2,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  // console.log(req.rawHeaders);
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  // console.log(req.rawHeaders);
+  res.send("<h1>Phone: 3207831341</h1>");
+});
+
+app.get("/about", (req, res) => {
+  // console.log(req.rawHeaders);
+  res.send("<h1>Gabriel Mejia</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
