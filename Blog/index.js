b9ea9c3f89859658ebc2ad6c016e5d9b6ede75e8
Gabriel P <gabopos@gmail.com>
2024-02-22 23:09:29 -0500

    Change project structure

--- Blog/index.js
+++ Blog/index.js
@@ -1,10 +1,20 @@
 import express from "express";
-import bodyParser from "body-parser";
+import bodyParser from "body-parser"; //forms
+import debug from "debug"; //debugging 
+import path from "path"; // path management
+import passport from "passport" //authentication
+
 
 const app = express();
 const port = 3000;
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/public/')));
+
+const postRouter = require('./views/routes/postRoutes')(nav);
+const homeRouter = require('./src/routes/homeRoutes');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/post', postRouter);
+app.use('/', homeRouter)
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
@@ -12,5 +22,5 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${chalk.green(port)}`);
 });
