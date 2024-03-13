9ddc11246a32773c4ea945e1494a6e177656c177
Gabriel P <gabopos@gmail.com>
2024-03-12 22:56:54 -0500

    Upload and setup MongoDB

--- NodeJs Blog/app.js
+++ NodeJs Blog/app.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
-app.use(express.static('public'));
+// Connect to Database
+connectDB();
+
+app.use(express.static("public"));
 
 //Templating Engine
 app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
-app.use('/', require('./server/routes/main'));
+app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => {
   console.log(`App Listening on port: ${PORT}`);
-})
+});
