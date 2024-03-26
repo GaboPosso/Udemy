2b93215f851085c74f627455f00d8220ea0f9d19
Gabriel P <gabopos@gmail.com>
2024-03-26 00:52:37 -0500

    Upload Admin page

--- NodeJs Blog/server/routes/admin.js
+++ NodeJs Blog/server/routes/admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 const adminLayout = "../views/layouts/admin";
 /**
@@ -22,6 +23,33 @@ router.get("/admin", async (req, res) => {
     console.log(error);
   }
   res.render("index", { locals });
+});
+
+/**
+ * POST /
+ * Admin  - Check Login
+ */
+
+
+router.post('/admin', async(req, res) => {
+  try {
+
+    const { username, password } = req.body;
+    // console.log(req.body)
+
+    if(req.body.username === 'admin' && req.body.password === 'password') {
+      res.send("You are logged in")
+    }
+    else {
+      res.send("Wrong username or password")
+    }
+    res.redirect('/admin')
+
+
+    res.render('admin/index', { locals, layout: adminLayout });
+  } catch {
+    console.log(error)
+  }
 });
 
 module.exports = router;
