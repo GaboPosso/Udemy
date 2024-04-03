1d02fb991131e1b447c82cc648839d9128176b29
Gabriel P <gabopos@gmail.com>
2024-04-03 00:07:47 -0500

    Add dashboard to database

--- NodeJs Blog/server/routes/admin.js
+++ NodeJs Blog/server/routes/admin.js
@@ -3,9 +3,33 @@ const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
+
+
+/**
+ * 
+ * Admin  - Check Login
+ *
+ */
+const authMiddleware = (req, res, next ) => {
+  const token = req.cookies.token;
+
+  if(!token) {
+    return req.status(401).json( { message: 'Unauthorized'});
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return req.status(401).json( { message: 'Unauthorized'})
+  }
+}
+
 /**
  * GET /
  * Admin  - Login Page
@@ -20,7 +44,7 @@ router.get("/admin", async (req, res) => {
 
   try {
     const data = await Post.find();
-    res.render('admin/index', { locals, layout: adminLayout});
+    res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
   }
@@ -32,59 +56,93 @@ router.get("/admin", async (req, res) => {
  * Admin  - Check Login
  */
 
-
-router.post('/admin', async(req, res) => {
+router.post("/admin", async (req, res) => {
   try {
-
     const { username, password } = req.body;
-    // console.log(req.body)
+    const user = await User.findOne({ username });
 
-    if(req.body.username === 'admin' && req.body.password === 'password') {
-      res.send("You are logged in")
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-    else {
-      res.send("Wrong username or password")
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-    res.redirect('/admin')
 
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
 
-    res.render('admin/index', { locals, layout: adminLayout });
+    res.redirect('/dashboard');
+
+    // if(req.body.username === 'admin' && req.body.password === 'password') {
+    //   res.send("You are logged in")
+    // }
+    // else {
+    //   res.send("Wrong username or password")
+    // }
+    // res.redirect('/admin')
+
+    // res.render('admin/index', { locals, layout: adminLayout });
   } catch {
-    console.log(error)
+    console.log(error);
   }
 });
+
+/**
+ * POST /
+ * Admin  - dashboard
+ */
+
+router.get("/dashboard", authMiddleware,  async (req, res) => {
+  res.render('admin/dashboard');
+ })
+
+// router.post('/admin', async(req, res) => {
+//   try {
+
+//     const { username, password } = req.body;
+
+//     if(req.body.username === 'admin' && req.body.password === 'password') {
+//       res.send("You are logged in")
+//     }
+//     else {
+//       res.send("Wrong username or password")
+//     }
+//     res.redirect('/admin')
+
+//     res.render('admin/index', { locals, layout: adminLayout });
+//   } catch {
+//     console.log(error)
+//   }
+// });
 
 /**
  * POST /
  * Admin  - Register
  */
 
-
-router.post('/register', async(req, res) => {
+router.post("/register", async (req, res) => {
   try {
-
     const { username, password } = req.body;
     // console.log(req.body)
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
-      const user = await User.create({username, password: hashedPassword});
-      res.status(201).json({ message: 'User Created', user});
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User Created", user });
     } catch (error) {
-      if(error.code === 11000) {
-        res.status(409).json({ message: 'User already in use'});
+      if (error.code === 11000) {
+        res.status(409).json({ message: "User already in use" });
       }
-      res.status(500).json({ message: 'Internal Sever Error'});
-      
+      res.status(500).json({ message: "Internal Sever Error" });
     }
 
-    res.render('admin/index', { locals, layout: adminLayout });
+    res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
-
-
-
 
 module.exports = router;
