a3797cdb689172ec77fe5cece708b57b1dfa4480
Gabriel Mejia <gabopos@gmail.com>
2024-07-18 13:00:27 -0500

    Add Blog List view and add blog functionality}

--- Backend/Posts Project/index.js
+++ Backend/Posts Project/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 3000;
 
@@ -7,8 +7,24 @@ const port = 3000;
 // const homePath = join(__dirname, "./views/partials/home.ejs");
 // const blogDetailsPath = join(__dirname, "./views/partials/blogDetails.ejs");
 
-app.get('/', (req, res) => {
- res.render('index.ejs');
+let blogList = [];
+
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/home", (req, res) => {
+  const blogTitle = req.body.blogTitle;
+  const blogDescription = req.body.blogDes;
+  blogList.push({
+    id: generateID(),
+    title: blogTitle,
+    description: blogDescription,
+  });
+  res.render("home.ejs", { blogList: blogList });
 });
 
 app.listen(port, () => {
