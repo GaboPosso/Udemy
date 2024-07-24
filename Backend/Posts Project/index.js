9ae1a59ea07211b99b701c5f18cb50955c3a610e
Gabriel Mejia <gabopos@gmail.com>
2024-07-23 23:38:14 -0500

    Review Express blog errors

--- Backend/Posts Project/index.js
+++ Backend/Posts Project/index.js
@@ -1,19 +1,35 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
 
-// const __dirname = dirname(fileURLtoPath(import.meta.url));
-// const indexPath = join(__dirname, "index.ejs");
-// const homePath = join(__dirname, "./views/partials/home.ejs");
-// const blogDetailsPath = join(__dirname, "./views/partials/blogDetails.ejs");
+//Configure Express middleware
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//Define paths to view files
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const indexPath = join(__dirname, "index.ejs");
+const homePath = join(__dirname, "./views/partials/home.ejs");
+const blogDetailsPath = join(__dirname, "./views/partials/blogDetails.ejs");
+
+//Initialize blog list
 let blogList = [];
 
-
+//Render index page
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render(indexPath);
 });
 
+//Render home page with blog list
+app.get("/home", (req, res) => {
+  res.render(homePath, { blogList: blogList });
+});
+
+//Add new blog
 app.post("/home", (req, res) => {
   const blogTitle = req.body.blogTitle;
   const blogDescription = req.body.blogDes;
@@ -22,12 +38,10 @@ app.post("/home", (req, res) => {
     title: blogTitle,
     description: blogDescription,
   });
-  res.render("./views/partials/home.ejs", { blogList: blogList });
+  res.render(homePath, { blogList: blogList });
 });
-function generateID() {
-  return Math.floor(Math.random() * 1000000);
-}
 
+//
 app.post("/edit/:id", (req, res) => {
   const blogId = req.params.id;
   const editBlog = blogList.findIndex((blog) => blog.id === parseInt(blogId));
@@ -44,21 +58,30 @@ app.post("/edit/:id", (req, res) => {
   res.render("home.ejs", { isEdit: true, blogList: blogList });
 });
 
-app.post('/delete/:id')
-
+//Render blog details page
 app.get("/blogDetails/:id", (req, res) => {
   const blogId = req.params.id;
   const blogDetails = blogList.find((blog) => blog.id === parseInt(blogId));
   res.render(blogDetails, { blogDetails: blogDetails });
 });
 
-app.post('/delete/:id', (req, res) => {
+// Delete blog
+app.post("/delete/:id", (req, res) => {
   const blogId = req.params.id;
   blogList = blogList.filter((blog) => blog.id !== parseInt(blogId));
-  res.send('<script>alert("Blog Deleted Succesfully"); window.location="/home";</script>');
-  res.redirect('/home');
+  res.send(
+    '<script>alert("Blog Deleted Succesfully"); window.location="/home";</script>'
+  );
+  res.redirect("/home");
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Function to generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
