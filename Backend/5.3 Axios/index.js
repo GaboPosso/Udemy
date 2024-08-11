2932e4253e568c215c757cd8964722eca19ce065
Gabriel Mejia <gabopos@gmail.com>
2024-08-10 23:53:17 -0500

    Start REST APIS Exercise

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-
+const URL = "https://bored-api-appbrewery.com/random";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,14 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const response = await axios.get(URL);
     const result = response.data;
     res.render("index.ejs", { data: result });
   } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
-    });
+    console.error("Failed to make request", error.message);
+    res.render("index.ejs", { error: error.message });
   }
 });
 
@@ -32,23 +30,7 @@ app.post("/", async (req, res) => {
   // sure you're passing both the type and participants queries.
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
-  try {
-    console.log(req.body);
-    const type = req.body.type;
-    const participants = req.body.participants;
-    const response = await axios.get(
-      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
-    );
-    const result = response.data;
-    res.render("index.ejs", {
-      data: result[Math.floor(Math.random() * result.length)],
-    });
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: "No activities that match your criteria.",
-    });
-  }
+
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
