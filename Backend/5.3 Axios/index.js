c6b419ce008966318ef840a4b9040bef03f9a806
Gabriel Mejia <gabopos@gmail.com>
2024-08-11 22:17:25 -0500

    Review AXIOS APIS requests

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const URL = "https://bored-api-appbrewery.com/random";
+const URL = "https://bored-api.appbrewery.com/random";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -24,12 +24,30 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   console.log(req.body);
-
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/filter", {
+      params: {
+        type: req.body.type,
+        participants: req.body.participants,
+      },
+    } );
+    const result = response.data;
+    res.render("index.ejs", { data: result[Math.floor(Math.random() * result.length)]});
+  } catch (error) {
+    console.error("Failed to make request", error.message);
+    res.render("index.ejs", { error: error.message })
+  }
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
+
+  try {
+    const response = await axios.post()
+  } catch (error) {
+    
+  }
 
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
